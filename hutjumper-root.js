0ff3419c337ee571cbd14dc08b4cd552d28e5149
Shawn Hussey <shawnhussey+github@gmail.com>
2014-03-16 17:41:41 -0230

    Started work on renderin/updating new ents...

--- hutjumper-root.js
+++ hutjumper-root.js
@@ -8,3 +8,27 @@ if (typeof HutJumper === 'undefined' || !HutJumper) {
 } else {
     console.error('"HutJumper" namespace is already defined.');
 }
+
+/**
+ *  Make one class extend another.
+ *
+ *  @param base {function} Base class constructor.
+ *  @param sub {function} Sub-class constructor.
+ */
+function extend(base, sub, properties) {
+  sub.prototype = Object.create(base.prototype);
+  sub.prototype.constructor = sub;
+  Object.defineProperty(sub.prototype, 'constructor', { 
+    enumerable: false, 
+    value: sub 
+  });
+  if (typeof properties !== 'undefined' && properties) {
+    for (propName in properties) {
+        sub.prototype[propName]
+        Object.defineProperty(sub.prototype, propName, { 
+            enumerable: true, 
+            value: properties[propName]
+        });
+    }
+  }
+}
