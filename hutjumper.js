576589af996ed4a8b60cd0d84b6c8f6a466f6bcb
Shawn Hussey <shawnhussey+github@gmail.com>
2014-03-21 11:51:23 -0230

    Added control scheme in main comments.

--- hutjumper.js
+++ hutjumper.js
@@ -1,6 +1,15 @@
 /**
  * Controls the drawing and interactivity of a side-scrolling game on an
  * HTML 5 canvas element.
+ *
+ * Control scheme:
+ *  W - up
+ *  S - down
+ *  A - left
+ *  D - right
+ *  SPACE - jump
+ *  MOUSE1 - shoot projectile
+ *  MOUSE2 - change character
  * 
  * @author Vahnatai
  */
